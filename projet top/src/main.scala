69c17d07bb36980fce16a8f94cfa0316669ea1f8
Roms11 <romaricm@wanadoo.fr>
2015-12-28 12:49:00 +0100

    Presque terminé :D

--- projet top/src/main.scala
+++ projet top/src/main.scala
@@ -67,11 +67,11 @@ object main extends App {
   }
   
   
-  def flouter(input:Array[Array[Int]],output:Array[Array[Int]]){
+  def flouter(input:Array[Array[Int]],output:Array[Array[Int]],size:Int){
     
     for(x<-0 to output(0).length-1){
       for(y<-0 to output.length-1){
-        output(y)(x) = getBWcolor(moyenne(6,x,y,input));
+        output(y)(x) = getBWcolor(moyenne(size,x,y,input));
       }
     }
     
@@ -94,6 +94,17 @@ object main extends App {
       }
     }
     return dst
+  }
+  
+  //Créer une image blanche
+  def blanche(src:Array[Array[Int]]){
+    
+    for(i<- 0 until src.length){
+      for (j<-0 until src(0).length){
+        src(i)(j)=0xFFFFFFFF;
+      }
+    }
+  
   }
   
  //fonction de transformation de l'image en gris
@@ -166,22 +177,182 @@ object main extends App {
 	
 	//// FIN SOBEL ////
   
+	
+	//// PARALLELISME ////
+	
+	def tracerligne(output:Array[Array[Int]],x:Int,y:Int,size:Int,angle:Int,color:Long){
+	  
+	  var newX = 0;
+	  var newY = 0;
+	  
+	  for(s<- 0 to size){
+      		      
+      newX = x + (s*Math.cos(angle*6.283/360)).toInt;
+      newY = y + (s*Math.sin(angle*6.283/360)).toInt;
+       
+	    if(newX>=0 && newX<output(0).length && newY>=0 && newY<output.length){
+        output(newY)(newX) = color.toInt;
+      }
+        
+    }
+	  
+	}
+	
+	def recherche_parallele(image:Array[Array[Int]],x:Int,y:Int,angle:Int,taille_recherche:Int):Array[Int]={
+	  
+	  var newXdist = 0;
+	  var newYdist = 0;
+	  var newX = 0;
+	  var newY = 0;
+	  var ligne = true;
+	  var resultat = Array(0,0,0);
+	  var blanc = 0;
+	  
+	  //On avance perpendiculairement
+	  for(distance <- 2 to taille_recherche*2){
+	    
+	    newXdist = x + (distance*Math.cos((angle+90)*6.283/360)).toInt;
+	    newYdist = y + (distance*Math.sin((angle+90)*6.283/360)).toInt;
+	    
+	    //Si le pixel destination est noir
+      if(lirePixel(newXdist,newYdist,image)<100){
+        
+        ligne = true;
+        
+        //On regarde si on a une ligne continue
+        for(taille<- 1 to taille_recherche){
+		      
+		      newX = newXdist + (taille*Math.cos(angle*6.283/360)).toInt;
+		      newY = newYdist + (taille*Math.sin(angle*6.283/360)).toInt;
+		       
+		      if(lirePixel(newX,newY,image)>100){
+		        ligne = false;
+		      }
+		        
+		    }
+        
+        if(ligne == true && blanc>1){
+          resultat(0) = 1;
+          resultat(1) = newXdist;
+          resultat(2) = newYdist;
+        }
+      
+      }else{
+        blanc = blanc+1;
+      }
+	    
+	  }
+	  
+	  return resultat;
+	}
+	
+	def chercherpara(image:Array[Array[Int]],output:Array[Array[Int]]){
+	  
+	  blanche(output);
+	  
+	  var taille_recherche = 10;
+	  var newX = 0;
+	  var newY = 0;
+	  var ligne = false;
+	  var parallele = Array(0,0,0);
+	  
+	  for(x <- taille_recherche to image(0).length-1-taille_recherche){
+	    
+	    println((x*100/image(0).length)+"%");
+	    
+		  for(y <- taille_recherche to image.length-1-taille_recherche){
+		    
+		    if(lirePixel(x,y,image)<100){
+		      
+  		    //Pour chaque pixel on regarde dans les 5 pixels environnants
+  		    for(angle<- 0 to 360 by 10){
+  		      
+  		      newX = x + (taille_recherche*Math.cos(angle*6.283/360)).toInt;
+  		      newY = y + (taille_recherche*Math.sin(angle*6.283/360)).toInt;
+  		       
+  		      //Si le pixel destination est noir
+  		      if(lirePixel(newX,newY,image)<100){
+  		        
+  		        ligne = true;
+  		        
+  		        //On regarde si on a une ligne continue
+  		        for(taille<- 1 to taille_recherche){
+      		      
+      		      newX = x + (taille*Math.cos(angle*6.283/360)).toInt;
+      		      newY = y + (taille*Math.sin(angle*6.283/360)).toInt;
+      		       
+      		      if(lirePixel(newX,newY,image)>100){
+      		        ligne = false;
+      		      }
+      		        
+      		    }
+  		        
+  		        if(ligne == true){
+  		          
+  		          parallele = recherche_parallele(image,x,y,angle,taille_recherche);
+  		          if(parallele(0)==1){
+  		            tracerligne(output,((x+parallele(1))/2).toInt,((y+parallele(2))/2).toInt,taille_recherche,angle,0xFFFF0000);
+  		          }
+  		        }
+  		      
+  		      }
+  		        
+  		    }
+		    
+		    }
+		    
+		  }
+	  }
+	  
+	  
+	}
+	
+	//// FIN PARALLELISME ////
   
   
   
   //// MAIN ////
  
   //Entree image
-	var filename : String = "assets/Images/ImagesTests/all.jpg"
+	var filename : String = "assets/Images/ImagesTests/1.jpg"
 	var wrappedInputImage : ImageWrapper = new ImageWrapper(filename);
 	var inputImage : Array[Array[Int]] = wrappedInputImage.getImage();
 	//Future image de sortie
 	var wrappedOutputImage : ImageWrapper = new ImageWrapper(filename);
 	var outputImage : Array[Array[Int]] = wrappedOutputImage.getImage();
 
-	flouter(inputImage,outputImage);
+	var outputFile:String="assets/present/0_init.jpg"
+	wrappedOutputImage.saveImage(outputFile)
+	
+	println("Flouter...");
+	flouter(inputImage,outputImage,6);
+	 
+	outputFile ="assets/present/1_flou.jpg"
+	wrappedOutputImage.saveImage(outputFile)
+	
+	println("Sobel...");
 	Sobel(outputImage);
 	
-	var outputFile:String="assets/all.jpg"
+	outputFile ="assets/present/2_sobel.jpg"
+	wrappedOutputImage.saveImage(outputFile)
+	
+	inputImage = copy(outputImage);
+	
+	println("Flouter légèrement...");
+	flouter(inputImage,outputImage,2);
+
+	outputFile ="assets/present/3_flou.jpg"
+	wrappedOutputImage.saveImage(outputFile)
+	
+	inputImage = copy(outputImage);
+	
+	println("Parallelismes");
+	chercherpara(inputImage,outputImage);
+
+	
+	
+	println("terminé");
+	
+	outputFile ="assets/present/4_fin.jpg"
 	wrappedOutputImage.saveImage(outputFile)
 }
